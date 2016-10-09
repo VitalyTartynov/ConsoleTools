c3efd556ffd9c681998c4d9c4b056f86f45bf075
Rubius\TartunovVA <Vitaliy.Tartunov@rubius.com>
2016-10-09 17:13:55 +0700

    Realized

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// /****************************** generate_guid ******************************\
+// Project:            generate_guid
+// Filename:           Program.cs
+// Created:            09.10.2016
+// 
+// <summary>
+// Простейшая консольная команда, создающая новый GUID и копирующая его в буфер обмена
+// </summary>
+// \***************************************************************************/
+
+using System;
+using System.Windows.Forms;
+
+namespace generate_guid
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            
+            Clipboard.SetText(id.ToString());
+            Console.WriteLine($"Guid copied to clipboard: {id}");
+        }
+    }
+}
