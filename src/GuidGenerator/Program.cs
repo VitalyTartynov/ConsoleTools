362b5afd9fdbc31cf1f6580c552ae04dd2aee09f
TartynovViA <TartynovViA@tomsknipi.ru>
2018-07-30 09:56:29 +0700

    Перенес бинарник из репозитория в раздел "Релизы". Реорганизовал структуру проекта. Перевел утилиту на .NET 4.6.1

--- src/GuidGenerator/Program.cs
+++ src/GuidGenerator/Program.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Windows.Forms;
 
-namespace generate_guid
+namespace GuidGenerator
 {
     class Program
     {
---
+++ src/GuidGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuidGenerator")]
+[assembly: AssemblyDescription("Консольная утилита, создающая новый GUID и копирующая его в буфер обмена")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tartynov Vitaly")]
+[assembly: AssemblyProduct("GuidGenerator")]
+[assembly: AssemblyCopyright("No copyright")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("56a28e6b-d940-48a7-a408-69518787e5ee")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
